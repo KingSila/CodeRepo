5a1020fe7b73396600a74ce5147096047abc2c27
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-04-15 16:29:17 +0200

    Day1 Javascript basics

---
+++ 01Basics/Arrays.js
@@ -0,0 +1,12 @@
+var customers = [250,1000,3];
+
+//console.log(customers[1]); // access the 2nd element of array
+
+console.log(customers[0]);   // first element in the arrray
+console.log(customers[1]);   // second element in the arrray
+console.log(customers[2]);   // third  element in the arrray
+console.log(customers.length);   // number of elements in array
+console.log(customers.sort);   // sort the elements in the Array
+console.log(customers[0]);   // first element in the arrray after sort
+console.log(customers[1]);   // second element in the arrray after sort
+console.log(customers[2]);   // third  element in the arrray after sort
---
+++ 01Basics/hello.js
@@ -0,0 +1,23 @@
+/* This is my first java script
+multi line comment
+*/
+console.log('Hello World!'); // in-line comment
+
+/*  Data types and variables */
+
+var myName ="Silas"
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+var a = 7;
+var b = 2 ;
+
+b = a;
+
+console.log(a)
+
+
+
+
