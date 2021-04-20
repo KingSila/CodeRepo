7639f3415f2303b9146270a20733dba6ef9580e9
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-04-20 18:31:23 +0200

    Day 6 Log

---
+++ CodingChallenges/TestGround.js
@@ -0,0 +1,12 @@
+var str = ";;;SILAS";
+//var res = str.substr(1, 25);
+var  check = str.startsWith()
+const  allowedLetters =  "^[0-9_]";
+
+//str.match(/[a-z]/i)
+
+
+//console.log(res.length);
+console.log(allowedLetters);
+console.log(check);
+console.log(str.match(/[a-z]/i));
