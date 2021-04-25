79398ac68eb5b8037064358c68f0df0784ffaf3e
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2021-04-25 13:09:05 +0200

    Day 10 Log

--- CodingChallenges/Scrap2.js
+++ CodingChallenges/Scrap2.js
@@ -5,23 +5,27 @@ var indices = [];
 var idx;
 var sorted = [];
 var strArr = [];
-var sortedfirstSet = [];
-var secondfirstSet = [];
+var sortedfirstSet ;
+var secondfirstSet ;
+var result = true;
+var matched = [];
 
 function FindIntersection(strArr)
 {
     sorted = Array.prototype.slice.call(strArr).sort();   
-    sortedfirstSet = sorted[0];
-    secondfirstSet = sorted[1];
+    sortedfirstSet = [].concat(sorted[0]);
+    secondfirstSet = [].concat(sorted[1]);
 
-    idx = sortedfirstSet.indexOf(secondfirstSet);
 
-    if(idx === -1) {
-    indices.push(idx);
-     idx = sortedfirstSet.indexOf(secondfirstSet, idx + 1);
-    }
-    
+
 }
 
 console.log(FindIntersection(["1,3,4,7,13", "1,2,4,13,15"]));
-console.log(indices);
+
+console.log("Sorted Array size " + sorted.length);
+console.log("1st Array size " + sortedfirstSet.length);
+console.log("1st Array  " + sortedfirstSet);
+console.log("2nd Array size " + secondfirstSet.length);
+console.log("2nd Array  " + secondfirstSet);
+console.log("RESULTS........... " + result);
+console.log("Match Array size " + indices.length);
